7151db308613a2beccf750830e7764abb8ed5f51
cleibertmora <cleibert95@outlook.com>
2019-08-06 08:28:35 -0500

    package installed

--- resources/views/templates/blog/posts/create.blade.php
+++ resources/views/templates/blog/posts/create.blade.php
@@ -2,6 +2,22 @@
 
 @section('content')
 
-hola
+    {{ Form::open(['route' => 'posts.store', 'method' => 'post', 'files' => true], ['class' => 'form']) }}
+
+        <div class="form-group">
+            {{ Form::label('title', 'URL amigable') }}
+            {{ Form::text('title', null, ['class' => 'form-control', 'id' => 'title']) }}
+        </div>
+
+        <div class="form-group">
+            {{ Form::label('body', 'Descripción') }}
+            {{ Form::textarea('body', null, ['class' => 'form-control']) }}
+        </div>
+
+        <div class="form-group">
+            {{ Form::submit('Guardar', ['class' => 'btn']) }}
+        </div>
+
+    {{ Form::close() }}
 
 @endsection
